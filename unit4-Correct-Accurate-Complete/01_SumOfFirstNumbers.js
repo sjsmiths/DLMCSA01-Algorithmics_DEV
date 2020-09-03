9156f997df1c828f451a4b72c1f41ab76883c75c
Paul Libbrecht <p.libbrecht@iubh-fernstudium.de>
2020-09-03 13:07:36 +0200

    Reviewed and copied till chapter 4. paul

--- unit4-Correct-Accurate-Complete/01_SumOfFirstNumbers.js
+++ unit4-Correct-Accurate-Complete/01_SumOfFirstNumbers.js
@@ -1,16 +1,17 @@
-const sum = (n) => {
+let readline = require('readline-sync');
+
+let sum = function (n){
     if (n>=0) {
-        let s = 0;
-        for (let i=1; i<=n; i++)
-            s = s + i;
+        let i = 0; let s = 0; for (let i=1; i<=n; i++){s = s + i;}
         return s;
     }
 }
 
-const readline = require('readline-sync');
-
 while (true){
-    let m = parseInt(readline.question("An integer please: "));
-    let sm = sum(m);
-    console.log((m+1) + ' first integers added: ' + sm + '\n');
+    let ms = readline.question("A whole number please: ");
+    let v = Number(ms);
+    if ((ms!=='') && (Number.isInteger(v)) && (v>=0)){
+        let m = parseInt(ms); let sm = sum(m); let o = 'first whole';
+        console.log('The '+(m+1)+' '+o+' numbers added : '+sm+'\n');
+    }
 }
